f8919e0ca0d84117e19767d6a47801f3dd6d64d5
carmona2511 <146292297+carmona2511@users.noreply.github.com>
2023-09-28 22:52:45 +0000

    Pending changes exported from your codespace

--- Ex_Files/02_01_begin/main.py
+++ Ex_Files/02_01_begin/main.py
@@ -1,13 +1,14 @@
-RUN_INDENTED = False
+RUN_INDENTED = True
+
+def my_function():
+    greet = "Hello"
+    return greet
 
 message = "running unindented"
 
 if RUN_INDENTED:
     message = "running indented"
 
+print(my_function())
 print(message)
 
-
-def my_function():
-    greet = "Hello"
-    return greet
